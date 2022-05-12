4f67626c0a0223368c3d632cf5fe4b978fbffbfe
oksey2021 <103522671+oksey2021@users.noreply.github.com>
2022-05-12 11:40:34 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,18 +2,15 @@
 
 export HOST_FILE="54.242.194.181"
 
-export PRIVATE_KEY_PATH="/Users/oluwaseunoke/Downloads/oksey2022-2.pem"
-
 export SSH_USER="ec2-user"
 
-export REMOTE_INSTALL_SCRIPT="sudo mv /home/ec2-user/indexes.conf /home/ec2-user/indexes
+export REMOTE_INSTALL_SCRIPT="/home/ec2-user/indexes.conf /home/ec2-user/indexes
 
 sudo cp /home/ec2-user/indexes.conf /home/splunk/
 
 sudo chown -R splunk:splunk /home/splunk
 
-sudo -u splunk diff /home/splunk/indexes /opt/splunk/etc/system/default/indexes.conf | grep ">" | cut -c 3- >>  /opt/splunk/etc/system/local/indexes.conf
-"
+sudo -u splunk diff /home/splunk/indexes /opt/splunk/etc/system/default/indexes.conf | grep '>' | cut -c 3- >>  /opt/splunk/etc/system/local/indexes.conf"
 
 
 ### ========================================== ###
@@ -35,7 +32,7 @@ echo "Starting."
 scp /var/lib/jenkins/workspace/firstJob/master-apps/all_indexes_user/local/indexes.conf $SSH_USER@$HOST_FILE:.
 echo "---------------------------"
 echo "Reading to the host"
-ssh  -t "$SSH_USER@$HOST_FILE" "$REMOTE_INSTALL_SCRIPT"
+ssh "$SSH_USER@$HOST_FILE" "$REMOTE_INSTALL_SCRIPT"
 #ssh -t "$i" "$REMOTE_INSTALL_SCRIPT"
 done
 echo "---------------------------"
